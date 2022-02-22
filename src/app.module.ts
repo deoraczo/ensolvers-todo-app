886f37b3ae21064f04bbfd8479ca9d1be22d66a0
deoraczo <deoraczo@gmail.com>
2022-02-22 11:56:24 -0500

    chore: connect to MySql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './modules/shared/shared.module';
 import { TaskingModule } from './modules/tasking/tasking.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import Config from './config';
 
 @Module({
-  imports: [SharedModule, TaskingModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      load: [Config]
+    }),
+    SharedModule, 
+    TaskingModule, 
+    DatabaseModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
