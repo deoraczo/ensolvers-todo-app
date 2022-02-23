c5ee4183425e2b0a15a5aa65f4edc8573d765cb4
deoraczo <deoraczo@gmail.com>
2022-02-23 07:07:33 -0500

    feat[filter]: add custom validation exception

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ValidationError } from 'class-validator';
 import { AppModule } from './app.module';
+import { ValidationException } from './modules/shared/exceptions/validation.exception';
+import { ValidationExceptionFilter } from './modules/shared/filters/validation-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  app.useGlobalFilters(new ValidationExceptionFilter())
+
+  app.useGlobalPipes(new ValidationPipe({
+    skipMissingProperties: false,
+    exceptionFactory: (erros: ValidationError[]) => {
+      const errMsg = {}
+      erros.forEach(err => {
+        errMsg[err.property] = [...Object.values(err.constraints)]
+      })
+
+      return new ValidationException(errMsg)
+    }
+  }))
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/tasking/tasks/services/create/create-task.dto.ts
+++ src/modules/tasking/tasks/services/create/create-task.dto.ts
@@ -1,3 +1,7 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class CreateTaskDTO {
+  @IsString()
+  @IsNotEmpty()
   title: string
 }
