be310c94f652d4d28a795d6fc251c0614c3ab154
deoraczo <deoraczo@gmail.com>
2022-02-24 01:24:39 -0500

    chore: config docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,14 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import Config from './config';
 import { enviroments } from './enviroments';
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/dist'),
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [Config]
